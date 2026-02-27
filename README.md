ad00e4a5aeefda8fe28b3bdd77710335ffd533fe
RanaUniverse <RanaUniverse321@gmail.com>
2026-02-27 20:17:30 +0530

    now i keep all

--- README.md
+++ README.md
@@ -9,7 +9,16 @@ Below will run the app in the port of 5001 in my host machine.
 docker run -p 5001:8001 rana_web_app
 ```
 
-to pass another folder name i will use 
+to pass another folder name i will use in the container i will want to have.
 ```
-docker build --build-arg APP_DIR=/ranauniverse -t image_tag_name .
+docker build --build-arg APP_DIR=/ranauniverse -t rana_web_app .
+```
+
+Now i will try to keep the caddy here in this project.
+First i will run the gunicorn server
+then i will run the caddy server which will use https and it will connect with reverse proxy.
+
+The below should start the server goodly.
+```
+docker compose up --build
 ```
--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 This is main.py file
 """
 
+import datetime
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,10 +12,29 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>This is My Website</h1>"
+    return f"""
+    Thanks you
+        <h1>Hello, Rana Universe!</h1>
+        <p>Handled by PID: {os.getpid()}</p>
+
+        <a href="/time">Click here to see current Indian time</a>
+    """
+
+
+@app.route("/time")
+def shows_now_time():
+    now_time_gmt = datetime.datetime.now(tz=datetime.timezone.utc)
+    now_time_ind = now_time_gmt + datetime.timedelta(hours=5, minutes=30)
+    return f"""
+    <a href="/">Go To Main Page</a>
+    <h1>Rana Universe</h1>
+    <h2>Current Indian Time: {now_time_ind}</h2>
+        <p>Handled by PID: {os.getpid()}</p>
+    """
 
 
 import sys
+
 print("Python Version is,", sys.version)
 print("Executable Location is,", sys.executable)
 
